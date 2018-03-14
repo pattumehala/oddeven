bcdc5c976a693b19fc393372ea8a9dcdd3f85321
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-03-14 16:11:10 +0545

    oddeven.C
    given a string print the odd and even position characters

--- oddeven.C
+++ oddeven.C
@@ -5,11 +5,19 @@ int main()
 char a[10],b[10];
 int i,n;
 printf("\nEnter the string:");
-scanf("%d",a);
+scanf("%s",a);
 n=strlen(a);
 for(i=0;i<n;i=i+2)
 {
 b[i]=a[i];
-printf("%s",b[i]);
+printf("%c",b[i]);
 }
 printf(" ");
+for(i=1;i<n;i=i+2)
+{
+b[i]=a[i];
+  printf("%c",b[i]);
+}
+  return 0;
+}
+    
